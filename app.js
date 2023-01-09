cf1db7266511a550478f9b2fccc7ca729cbf66ac
Kirstin <kpetersen@elon.edu>
2023-01-09 09:12:30 +0000

    livesLost, scared-baddies, removing baddies from the ghome

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ let box;
 let pokeBallPosition;
 let score = 0;
 const scoreDisplay = document.querySelector(".scoreDisplay");
+const directionOptions = [+1, -1, +width, -width];
+let movement =
+  directionOptions[Math.floor(Math.random() * directionOptions.length)];
 
 const boardDesign = [
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -104,11 +107,9 @@ function move() {
     !boxes[pokeBallPosition + 1].classList.contains("wall")
   ) {
     pokeBallPosition++;
-    // console.log("start", pokeBallPosition);
     if (pokeBallPosition === 419) {
       pokeBallPosition = 392;
     }
-    // console.log("end", pokeBallPosition);
   } else if (
     event.code === "ArrowLeft" &&
     x !== 0 &&
@@ -131,25 +132,39 @@ function move() {
   ) {
     pokeBallPosition -= width;
   }
-  PokemomAndEngergiserEaten();
   boxes[pokeBallPosition].classList.add("pokeball");
+  pokemomEaten();
+  engergiserEaten();
+  loseLife();
 }
 
 document.addEventListener("keydown", move);
 
-function PokemomAndEngergiserEaten() {
+function pokemomEaten() {
   if (boxes[pokeBallPosition].classList.contains("pokemon")) {
     boxes[pokeBallPosition].classList.remove("pokemon");
     score += 10;
     scoreDisplay.innerHTML = score;
-  } else if (boxes[pokeBallPosition].classList.contains("energiser")) {
-    boxes[pokeBallPosition].classList.remove("energiser");
-    // make baddies flash
-    // pacman can't be eatten
-    // ghost return home
   }
 }
 
+function engergiserEaten() {
+  if (boxes[pokeBallPosition].classList.contains("energiser")) {
+    boxes[pokeBallPosition].classList.remove("energiser");
+    // make the baddies flash
+    allBaddies.forEach((baddie) => (baddie.isScared = true));
+    setTimeout(revertBaddies, 30000);
+    // startBaddiesMovement();
+    // change to 10000 once I am done or don't need to fix scarred ghosts stuff
+  }
+}
+
+function revertBaddies() {
+  allBaddies.forEach((baddie) => (baddie.isScared = false));
+  console.log("reverted");
+}
+
+// random movement of ghosts - this works, don't delete ---------
 class Baddie {
   constructor(className, startPosition, speed) {
     this.className = className;
@@ -157,6 +172,7 @@ class Baddie {
     this.speed = speed;
     this.position = startPosition;
     this.timerId = 0;
+    this.isScared = false;
   }
 }
 
@@ -170,36 +186,279 @@ const james = new Baddie("james", 433, 500);
 const blue = new Baddie("blue", 434, 600);
 
 const allBaddies = [meowth, jessie, james, blue];
-
 allBaddies.forEach((baddie) => {
   boxes[baddie.position].classList.add(baddie.className);
   boxes[baddie.position].classList.add("baddies");
-  // boxes[baddie.position].timerId = 1000;
 });
 
-document.addEventListener("keydown", startBaddiesMovement);
-
+const exitLaneNumbs = [349, 377, 405, 350, 378, 406, 433, 434];
 function moveAllBaddies(baddie) {
-  const directionOptions = [+1, -1, +width, -width];
-  let movement =
-    directionOptions[Math.floor(Math.random() * directionOptions.length)];
-
   baddie.timerId = setInterval(function () {
-    if (
+    if (exitLaneNumbs.includes(baddie.position)) {
+      boxes[baddie.position].classList.remove(
+        baddie.className,
+        "baddies",
+        "scared-baddies"
+      );
+      baddie.position -= width;
+      boxes[baddie.position].classList.add(baddie.className, "baddies");
+    } else if (
       !boxes[baddie.position + movement].classList.contains("wall") &&
-      !boxes[baddie.position + movement].classList.contains("baddies")
+      !boxes[baddie.position + movement].classList.contains("baddies") &&
+      !boxes[baddie.position + movement].classList.contains("gHome")
     ) {
-      boxes[baddie.position].classList.remove(baddie.className, "baddies");
+      boxes[baddie.position].classList.remove(
+        baddie.className,
+        "baddies",
+        "scared-baddies"
+      );
       baddie.position += movement;
       boxes[baddie.position].classList.add(baddie.className, "baddies");
     } else {
       movement =
         directionOptions[Math.floor(Math.random() * directionOptions.length)];
     }
+
+    if (baddie.isScared === true) {
+      boxes[baddie.position].classList.add("scared-baddies");
+    }
+
+    if (
+      boxes[baddie.position].classList.contains("pokeball") &&
+      baddie.isScared === true
+    ) {
+      clearInterval(baddie.timerId);
+      boxes[baddie.position].classList.remove(
+        baddie.className,
+        "baddies",
+        "scared-baddies"
+      );
+      boxes[baddie.startPosition].classList.add(baddie.className, "baddies");
+      score += 100;
+    }
+
+    //   // setTimeout(moveAllBaddies(), 2000);
+    //   // returns the bad guys to their home  but doesn't restart their movement again - need to fix
+    // }
+    loseLife();
   }, baddie.speed);
 }
+
+function loseLife(baddie) {
+  //   // if the score = 2930 - You win! Would you like to play again?
+  //   // if endGame button is clicked - Are you sure you want to quit? Y/N options
+  //   // if you loose 3 lives - You loose
+  if (
+    boxes[pokeBallPosition].classList.contains("baddies") &&
+    !boxes[pokeBallPosition].classList.contains("scared-baddies")
+  ) {
+    allBaddies.forEach(function (baddie) {
+      clearInterval(baddie.timerId);
+      boxes[baddie.position].classList.remove(
+        baddie.className,
+        "baddies",
+        "scared-baddies"
+      );
+      baddie.position = baddie.startPosition;
+      boxes[baddie.position].classList.add(baddie.className, "baddies");
+    });
+    boxes[pokeBallPosition].classList.remove("pokeball");
+    pokeBallPosition = 489;
+    boxes[pokeBallPosition].classList.add("pokeball");
+    document.addEventListener("keydown", startBaddiesMovement);
+    document.addEventListener("keydown", move);
+
+    if (document.querySelector("#life3").classList.contains("life")) {
+      document.querySelector("#life3").innerHTML = "  ";
+      document.querySelector("#life3").classList.remove("life");
+    } else if (
+      !document.querySelector("#life3").classList.contains("life") &&
+      document.querySelector("#life2").classList.contains("life")
+    ) {
+      document.querySelector("#life2").innerHTML = "  ";
+      document.querySelector("#life2").classList.remove("life");
+    } else if (!document.querySelector("#life2").classList.contains("life")) {
+      document.querySelector("#life1").innerHTML = "  ";
+      document.querySelector("#life1").classList.remove("life");
+      endGame();
+    }
+  } else if (boxes[pokeBallPosition].classList.contains("scared-baddies")) {
+    console.log("ghost is scared");
+    // engergiserEaten();
+  }
+}
+
+function endGame() {
+  console.log("You have lost all your lives, game over!");
+  scoreDisplay.innerHTML = "You have no more lives. Game Over!";
+  boxes[pokeBallPosition].classList.remove("pokeball");
+  document.removeEventListener("keydown", move);
+
+  // SetTimeout - after 5 seconds returnt to the landing page?
+}
+// scoreDisplay.innertHTML = "Game Over!";
+
+// remove all ghosts and return to home
+// +
+// remove and return pokeball to starting position
+
+// if (score >= 2930 && !boxes.classList.contain("pokemon")) {
+//   console.log("endGame");
+// }
+
+// WORKS DONT DELETE ---------
+// function moveAllBaddies(baddie) {
+//   baddie.timerId = setInterval(function () {
+//     if (
+//       !boxes[baddie.position + movement].classList.contains("wall") &&
+//       !boxes[baddie.position + movement].classList.contains("baddies")
+//     ) {
+//       boxes[baddie.position].classList.remove(
+//         baddie.className,
+//         "baddies",
+//         "scared-baddies"
+//       );
+//       baddie.position += movement;
+//       boxes[baddie.position].classList.add(baddie.className, "baddies");
+//     } else {
+//       movement =
+//         directionOptions[Math.floor(Math.random() * directionOptions.length)];
+//     }
+
+//     if (baddie.isScared === true) {
+//       boxes[baddie.position].classList.add("scared-baddies");
+//     }
+
+//     if (
+//       boxes[baddie.position].classList.contains("pokeball") &&
+//       baddie.isScared === true
+//     ) {
+//       clearInterval(baddie.timerId);
+//       boxes[baddie.position].classList.remove(
+//         baddie.className,
+//         "baddies",
+//         "scared-baddies"
+//       );
+//       boxes[baddie.startPosition].classList.add(baddie.className, "baddies");
+//       score += 100;
+
+//       // setTimeout(moveAllBaddies(), 2000);
+//       // returns the bad guys to their home  but doesn't restart their movement again - need to fix
+//     }
+//     // endGame();
+//   }, baddie.speed);
+// }
+
+// ----------
 
 function startBaddiesMovement() {
   allBaddies.forEach((baddie) => moveAllBaddies(baddie));
   document.removeEventListener("keydown", startBaddiesMovement);
 }
+
+// --------------
+// Make baddies move out of box and onto the board and then start random movement.
+// if (baddie.position = 321, 349, 377, 405, 406, 433, 434)
+// if (baddie.position = 405 || 433) - minus 28 three times and then start random movement
+// if (baddie.position = 406 || 434) - minus 28 three times and then start random movement
+
+document.addEventListener("keydown", startBaddiesMovement);
+
+document.querySelector(".quitBtn").addEventListener("click", (endGame) => {
+  console.log("connected");
+});
+
+// ------DONT TOUCH BELOW---------- RETURN TO WHEN YOU WANT TO WORK ON LOGICAL MOVEMENT OF THE GHOSTS
+
+// class Baddie {
+//   constructor(className, startPosition, speed) {
+//     this.className = className;
+//     this.startPosition = startPosition;
+//     this.speed = speed;
+//     this.position = startPosition;
+//     this.timerId = 0;
+//     this.x = 0;
+//     this.y = 0;
+//   }
+// }
+
+// // // meowth = blinky (follows pac-man direclty)
+// // // jessie = inky (His target is relative to both Blinky and Pac-Man, where the distance Blinky is from Pinky's target is doubled to get Inky's target.)
+// // // james = pinky (chases to the 2 pac-dots in front of pac-man)
+// // // blue = clyde (chases directly after pacman)
+// const meowth = new Baddie("meowth", 405, 300);
+// // // const jessie = new Baddie("jessie", 406, 400);
+// // // const james = new Baddie("james", 433, 500);
+// // // const blue = new Baddie("blue", 434, 600);
+
+// // // const allBaddies = [meowth, jessie, james, blue];
+// const allBaddies = [meowth];
+
+// // console.log(meowth.position);
+
+// allBaddies.forEach((baddie) => {
+//   boxes[baddie.position].classList.add(baddie.className);
+//   boxes[baddie.position].classList.add("baddies");
+// });
+
+// function getCoordinates(index) {
+//   return [index % width, Math.floor(index / width)];
+//   // The first number returns the x coordinate and the second number returns the y coordinate of the relevant index
+// }
+
+// COME BACK TO THIS FUNCTION ONCE YOU GET MOST OF THE GAME WORKING -----------
+// function moveAllBaddies(baddie) {
+//   const directionOptions = [+1, -1, +width, -width];
+//   let movement =
+//     directionOptions[Math.floor(Math.random() * directionOptions.length)];
+//   baddie.timerId = setInterval(function () {
+//     if (
+//       !boxes[baddie.position + movement].classList.contains("wall") &&
+//       !boxes[baddie.position + movement].classList.contains("baddies")
+//     ) {
+//       const [pokeBallX, pokeBallY] = getCoordinates(pokeBallPosition);
+//       // console.log(`pokeball location: ${[pokeBallX, pokeBallY]}`);
+//       const [baddieX, baddieY] = getCoordinates(baddie.position);
+//       // console.log(`baddie location: ${[baddieX, baddieY]}`);
+//       const [newBaddieX, newBaddieY] = getCoordinates(
+//         baddie.position + movement
+//       );
+//       // console.log(`new baddie location: ${[newBaddieX, newBaddieY]}`);
+//       function isXCloser() {
+//         if (newBaddieX - pokeBallX > baddieX - pokeBallX) {
+//           return true;
+//         }
+//         return false;
+//       }
+//       // console.log(`x =${isXCloser()}`);
+
+//       function isYCloser() {
+//         if (newBaddieY - pokeBallY > baddieY - pokeBallY) {
+//           return true;
+//         }
+//         return false;
+//       }
+//       // console.log(`y =${isYCloser()}`);
+//       if (isXCloser() === true || isYCloser() === true) {
+//         console.log("correct coord.");
+//         boxes[baddie.position].classList.remove(baddie.className, "baddies");
+//         baddie.position += movement;
+//         boxes[baddie.position].classList.add(baddie.className, "baddies");
+
+// THIS WORKS UNTIL YOU HIT A WALL AND THEN IT DOESNT MOVE AFTER THAT, PROBLEM HERE?
+//       } else {
+//         movement =
+//           directionOptions[Math.floor(Math.random() * directionOptions.length)];
+//       }
+//     }
+//   }, baddie.speed);
+// }
+
+// function startBaddiesMovement() {
+//   allBaddies.forEach((baddie) => moveAllBaddies(baddie));
+//   document.removeEventListener("keydown", startBaddiesMovement);
+// }
+
+// document.addEventListener("keydown", startBaddiesMovement);
+
+// ------------------------
